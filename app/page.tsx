b233bd5f1bdc982d3e03864973a5c9a512e5dbb9
Samuel Korkoyah <skorkoyah29@gmail.com>
2025-12-27 13:29:14 -0500

    Implement Homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {Button} from "@/components/ui/button";
 import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 import Cta from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
@@ -40,7 +41,11 @@ const Page = () => {
       </section>
 
       <section className="home-section">
-          <CompanionList />
+          <CompanionList
+            title="Recently completed sessions"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+          />
           <Cta />
       </section>
     </main>
